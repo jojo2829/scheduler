5f15860b1562655bd151da7fda3542c7e7604e98
cy <cybc00@hotmail.com>
2021-07-06 01:27:11 -0700

    selected logic

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,11 +2,16 @@ import React from "react";
 import DayListItem from "components/DayListItem";
 
 export default function DayList(props){
-  const {days, day, setDay} = props;
+  const {days, setDay} = props;
 
   const mappedDays = days.map(day => {
+    if (day.name === props.day) {
+      return <ul key={day.id}>
+      <DayListItem name={day.name} spots={day.spots} setDay={setDay} selected/>
+      </ul>
+    }
     return <ul key={day.id}>
-      <DayListItem name={day.name} spots={day.spots} setDay={setDay}/>
+      <DayListItem name={day.name} spots={day.spots} setDay={setDay} />
     </ul>
   });
 
