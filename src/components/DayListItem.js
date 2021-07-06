5d01eb4dc33f6afc5884a1a2566094ef16ef2c73
cy <cybc00@hotmail.com>
2021-07-06 00:45:45 -0700

    daylistitem component function

---
+++ src/components/DayListItem.js
@@ -0,0 +1,28 @@
+import React from "react";
+import classnames from 'classnames';
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  const { name, spots, setDay, selected } = props;
+  let spotsString = `${spots} spots remaining`;
+
+  const dayListClass = classnames(
+    "day-list__item",
+    {"day-list__item--selected": selected},
+    {"day-list__item--full": spots ? 0 : false}
+  )
+
+  if (spots === 1) {
+    spotsString = "1 spot remaining";
+  }
+  if (spots === 0) {
+    spotsString = "no spots remaining";
+  }
+
+  return (
+    <li className={dayListClass} onClick={() => setDay(name)}>
+      <h2 className="text--regular">{name}</h2> 
+      <h3 className="text--light">{spotsString}</h3>
+    </li>
+  );
+}
