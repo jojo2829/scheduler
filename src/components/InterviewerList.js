dd12155c2599c9d3e57931b5e0952333a6a926c3
cy <cybc00@hotmail.com>
2021-07-06 18:44:56 -0700

    refactor to onChange for setInterviewer

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,7 +3,7 @@ import "components/InterviewerList.scss";
 import InterviewerListItem from "./InterviewerListItem";
 
 export default function InterviewerList(props) {
-  const { interviewers, setInterviewer } = props;
+  const { interviewers, value, onChange } = props;
 
   const mappedInterviewers = interviewers.map(interviewer => {
     return  <InterviewerListItem 
@@ -11,8 +11,8 @@ export default function InterviewerList(props) {
       id={interviewer.id} 
       name={interviewer.name}
       avatar={interviewer.avatar}
-      selected={interviewer.id === props.interviewer}
-      setInterviewer={(event) => setInterviewer(interviewer.id)}
+      selected={interviewer.id === value}
+      setInterviewer={(event) => onChange(interviewer.id)}
     />
   });
 
