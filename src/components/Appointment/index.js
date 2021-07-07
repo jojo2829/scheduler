40946128e8a77f1aab987661232ad829b73d560e
cy <cybc00@hotmail.com>
2021-07-06 20:11:32 -0700

    show appointment if there's interview else show add button

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,11 +2,17 @@ import React from "react";
 
 import "components/Appointment/styles.scss";
 
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+
 export default function Appointment(props) {
+  const {interview} = props;
 
   return (
     <article className="appointment">
-
+      <Header time={props.time} />
+      {interview ? <Show student={interview.student} interviewer={interview.interviewer} /> : <Empty />}
     </article>
   )
 };
