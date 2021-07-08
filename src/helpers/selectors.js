5f9ce1e608d08fd01608025e1de8025aab977174
cy <cybc00@hotmail.com>
2021-07-08 16:44:00 -0700

    get interviewers for the day

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -43,3 +43,30 @@ export function getInterview(state, interview) {
     interviewer
   };
 }
+
+export function getInterviewersForDay(state, day) {
+  let result = [];
+
+  if (!state.days.length === 0) {
+    return [];
+  }
+
+  const apptDate = state.days.filter(date => date.name === day);
+
+  if (!apptDate[0]) {
+    return [];
+  }
+  
+  const interviewerArr = apptDate[0].interviewers;
+  
+  for (const index in state.interviewers) {
+    const interviewer = state.interviewers[index];
+    for (const key of interviewerArr) {
+      if (key === interviewer.id) {
+        result.push(interviewer);
+      }
+    }
+  }
+
+  return result;
+}
