cb7086e2c7285a7dc1e7de129b5e2717e72cb380
cy <cybc00@hotmail.com>
2021-07-12 21:50:35 -0700

    fixed replace in transition

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,19 +1,25 @@
 import { useState } from "react";
 
 export default function useVisualMode (initial) {
+
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
   const transition = function (newMode, replace = false) {
 
-    if (replace) {
-      setHistory(prev => ([...prev, newMode]))
+    if(replace) {
+      history.pop()
+      setHistory(history);
     }
-    history.push(newMode);
+      
+    setHistory(prevArray => [...prevArray, newMode]);
     setMode(newMode);
+
   };
 
   const back = function () {
+
+    console.log('back called');
 
     if (history.length === 1) {
       setMode(initial)
@@ -21,7 +27,7 @@ export default function useVisualMode (initial) {
       history.pop();
       setMode(history[history.length - 1]);
     }
-  
+
   };
 
   return { mode, transition, back };
