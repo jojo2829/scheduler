21a54905bd08f7f62f885db4242a1883fb964924
cy <cybc00@hotmail.com>
2021-07-14 18:45:37 -0700

    edit appointment test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -8,18 +8,30 @@ describe("Appoinments", () => {
   });
 
   it("should book an interview", () => {
-    cy.get("[alt=Add]")
-    .first()
-    .click();
+    cy.get("[alt=Add]").first().click();
 
     cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones")
 
-    cy.get("[alt='Sylvia Palmer']").click()
+    cy.get("[alt='Sylvia Palmer']").click();
 
-    cy.contains("Save").click()
+    cy.contains("Save").click();
 
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+
+  it("should edit an interview", () => {
+    cy.get("[alt='Edit']")
+    .first()
+    .click({force: true});
+
+    cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");
+    cy.get("[alt='Tori Malcolm']").click();
+
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
   });
 
 });
