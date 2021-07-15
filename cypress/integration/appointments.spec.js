4f04002f3440fa4fbd551dec50c70f46d8129363
cy <cybc00@hotmail.com>
2021-07-14 19:01:42 -0700

    delete test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -46,6 +46,25 @@ describe("Appoinments", () => {
 
     cy.contains(".appointment__card--show", "Archie Cohen");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
+
+    cy.get("[alt=Add]").first().click();
+    cy.contains("Cancel").click();
+
+    cy.get("[alt='Add']").should("exist");
+  });
+
+  it("should delete an interview", () => {
+    cy.get("[alt='Delete']")
+    .first()
+    .click({force: true});
+
+    cy.contains("Confirm").click();
+
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+
+    cy.contains(".appointment__card--show", "Archie Cohen")
+    .should("not.exist");
   });
 
 });
